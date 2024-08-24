e7c3a809e57379389d08d024094e06252f44f15c
fsaile <fortunatocupons@gmail.com>
2024-08-24 15:30:09 -0300

    Atualiza app.py com as últimas modificações

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template
 import csv
 import os
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
@@ -15,16 +16,19 @@ def submit():
     name = request.form['name']
     email = request.form['email']
     password = request.form['password']
+    
+    # Criptografa a senha
+    hashed_password = generate_password_hash(password)
 
     # Adiciona os dados ao arquivo CSV
     if not os.path.isfile('submissions.csv'):
         with open('submissions.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['Name', 'Email', 'Password'])
-    
+
     with open('submissions.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([name, email, password])
+        writer.writerow([name, email, hashed_password])
 
     return redirect('/thankyou')
 
