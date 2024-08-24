752c21ffbf4f998030b7e3904f84301b3a5281f7
fsaile <fortunatocupons@gmail.com>
2024-08-24 16:05:34 -0300

    Reverte para a versão anterior do app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, request, redirect, render_template, flash  # Adicionado
+from flask import Flask, request, redirect, render_template
 import csv
 import os
-from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta_aqui'  # Necessário para mensagens flash
 
 @app.route('/')
 def index():
@@ -16,14 +14,6 @@ def submit():
     email = request.form['email']
     password = request.form['password']
 
-    # Validações
-    if not name or not email or not password:
-        flash('Todos os campos são obrigatórios.')  # Adicionado
-        return redirect('/')  # Retorna para o formulário em caso de erro
-
-    # Criptografa a senha
-    hashed_password = generate_password_hash(password, method='sha256')
-
     # Adiciona os dados ao arquivo CSV
     if not os.path.isfile('submissions.csv'):
         with open('submissions.csv', mode='w', newline='') as file:
@@ -32,10 +22,13 @@ def submit():
     
     with open('submissions.csv', mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([name, email, hashed_password])
+        writer.writerow([name, email, password])
 
     return redirect('/thankyou')
 
-@app.route('/thankyou')
+@app.route('/thankyou')c
 def thankyou():
     return render_template('thankyou.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
